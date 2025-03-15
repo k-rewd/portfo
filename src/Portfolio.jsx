14d1b58333345d1a05b5a79f7d29fe574a60a4e6
andrew <a.kimswk@gmail.com>
2025-03-15 16:29:17 -0700

    wtf

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,17 +1,18 @@
 import { Parallax } from "react-scroll-parallax";
+import './styles.css'
 
 export default function Portfolio() {
   return (
     <>
       <div className="parallax_wrapper">
-        <div className="parallax_group intro_screen">
+        <div className="parallax_group intro_screen" id="intro">
         <h1>Section 1</h1>
-          <div className="parallax_layer base_layer">1base</div>
+          <div className="parallax_layer base_layer" >1base</div>
           <div className="parallax_layer mid_layer">1mid</div>
           <div className="parallax_layer top_layer">1top</div>
 
         </div>
-        <div className="parallax_group">
+        <div className="parallax_group" id="section-2">
 
           <h1>Section 2</h1>
           <div className="parallax_layer base_layer">2base</div>
@@ -19,7 +20,7 @@ export default function Portfolio() {
           <div className="parallax_layer top_layer">2top</div>
 
         </div>
-        <div className="parallax_group">
+        <div className="parallax_group" id="section-3">
 
           <h1>Section 3</h1>
           <div className="parallax_layer base_layer">3base</div>
@@ -27,9 +28,9 @@ export default function Portfolio() {
           <div className="parallax_layer top_layer">3top</div>
 
         </div>
-        <div className="parallax_group outro_screen">
+        <div className="parallax_group outro_screen" id="outro">
 
-          <h1>Section 4</h1>
+          <h1>Outro</h1>
 
         </div>
       </div>
