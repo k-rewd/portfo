cdc8eb5e23f78f08b562865dfc5eca7d85b23985
andrew <a.kimswk@gmail.com>
2025-03-17 02:40:50 -0700

    going with diff approach

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -1,13 +1,15 @@
+import "./Home.css";
+
 export default function Home() {
   return (
-  <div className="parallax_wrapper">
-    <div className="parallax_group intro_screen" id="intro">
-      <h1>Section 1</h1>
-      <div className="parallax_layer base_layer" >1base</div>
-      <div className="parallax_layer mid_layer">1mid</div>
-      <div className="parallax_layer top_layer">1top</div>
-
-    </div>
+    <div className="parallax_wrapper">
+      {/* Section 1 */}
+      <div className="parallax_group">
+        <h1>Section 1</h1>
+        <div className="parallax_layer base_layer">1base</div>
+        <div className="parallax_layer mid_layer">1mid</div>
+        <div className="parallax_layer top_layer">1top</div>
+      </div>
   </div>
 )}
 
--- src/sections/Projects/ProjectA.jsx
+++ src/sections/Projects/ProjectA.jsx
@@ -1,8 +1,11 @@
+
+import './Projects.css'
+
 export default function ProjectA() {
   return (
   <div className="parallax_wrapper">
     <div className="parallax_group intro_screen" id="intro">
-      <h1>Section 1</h1>
+      <h1>Section 2</h1>
       <div className="parallax_layer base_layer" >1base</div>
       <div className="parallax_layer mid_layer">1mid</div>
       <div className="parallax_layer top_layer">1top</div>
